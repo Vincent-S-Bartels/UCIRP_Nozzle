225de39b1c7416a2840292cfaf4c81848d9c6ea4
Vincent-S-Bartels <vbartels@uci.edu>
2022-04-20 15:36:14 -0700

    Fixed Export Functionality

--- nozzleDimensionsExport.m
+++ nozzleDimensionsExport.m
@@ -1,9 +1,8 @@
 clear all; clc; close all;
 %% Inputs and constants
-OFratio = linspace(2.5, 3.5, 21); % All OFratio in CEA
+OFratio=input('Enter O/F wt. ratio (2.50:.05:3.50):');
 cPressure =  550;
 cPressure = cPressure * 6894.757; %psi to pascal
-
 tRadius =input('Enter throat RADIUS in inches:');    % inches --later converted to metric
 
 tRadius = tRadius*0.0254; % inches to meters
@@ -13,23 +12,63 @@ cArea = 3.5*tArea; % m^2
 cRadius = sqrt(cArea/pi); % m
 phi = 50; %pressure ratio
 
+
+[massDot, gamma] = getMassFlowRate(OFratio, 550, tArea);
+% returns the mass flow rate of the engine and average gamma between
+% throat and exit
+machExit = sqrt((phi^((gamma - 1)/gamma) - 1)*(2/(gamma - 1)) );
+%uses isentropic flow relation for pressure and pressure ratio phi
+epsilon = (1/machExit) * ((1 + ((gamma - 1)/2) * machExit^2)/...
+    ((gamma+1)/2))^((gamma + 1)/(2*(gamma-1)));
+%area ratio of exit to throat, based on isentropic flow relations
+eArea = tArea * epsilon; % m^2
+eRadius = sqrt(eArea/pi); % m
+
 AngleforLength = [5, 15, 30, 45, 60]; % choosing to vary the enterance length
 inletLength = (cRadius - tRadius)./tand(AngleforLength); % meters, triangle relation
-inletThroat = (1/8)/39.37008; % meters
-outletLength = (2*tRadius * (sqrt(epsilon) - 1)) /(2*tand(thetaOutletCone)); % meters, formula from M&ToP
+throatLength = (1/8)/39.37008; % meters
+thetaOutletCone = 15;
+outletLength = (2*tRadius * (sqrt(epsilon) - 1)) /(2*tand(thetaOutletCone)) * 0.8; % meters, formula from M&ToP
+% 80% truncated cone nozzle.
 
-%% Loop:
-for i = 0: length(OFratio)
-    [massDot, gamma] = getMassFlowRate(OFratio, cPressure, tArea);
-    % returns the mass flow rate of the engine and average gamma between
-    % throat and exit
-    machExit = sqrt((phi^((gamma - 1)/gamma) - 1)*(2/(gamma - 1)) );
-    %uses isentropic flow relation for pressure and pressure ratio phi
-    epsilon = (1/machExit) * ((1 + ((gamma - 1)/2) * machExit^2)/...
-        ((gamma+1)/2))^((gamma + 1)/(2*(gamma-1)));
-    %area ratio of exit to throat, based on isentropic flow relations
-    eArea = tArea * epsilon; % m^2
-    eRadius = sqrt(eArea/pi); % m
-    %% Nozzle Design
+theta1 = -15;  % angle from chamber to inlet
+theta2 = 0;    % angle from inlet to throat, needs to be 0
+theta3 = 0;    % angle from throat into outlet
+theta4 = 0;   % angle from outlet to exit
 
+
+%% Nozzle Design Loop:
+for j = 1: length(AngleforLength)
+    x = linspace(0, (inletLength(j) + throatLength + outletLength), 2000);
+    y = zeros(length(x), 1)';
+    L1 = inletLength(j) + throatLength;
+    LT = L1 + outletLength;
+    for i = 1:length(x)
+        if x(i) < inletLength(j)
+            m = tand(theta2) - tand(theta1);
+            a = cRadius;
+            b = tand(theta1);
+            c = (3/inletLength(j)^2)*(tRadius - cRadius - tand(theta1)*inletLength(j) - (1/3)*inletLength(j)*m);
+            d = m/(3*inletLength(j)^2) - 2*c/(3*inletLength(j));
+            y(i) = a + b*(x(i)) + c*(x(i))^2 + d*(x(i))^3;
+        elseif inletLength(j) <= x(i) && x(i) <(throatLength + inletLength(j))
+            y(i) = tRadius;
+        elseif (throatLength + inletLength(j)) <= x(i) && x(i) <= (inletLength(j) + throatLength + outletLength)
+            m = tand(theta4) - tand(theta3);
+            a = tRadius;
+            b = tand(theta3);
+            c = (3/outletLength^2)*(eRadius - tRadius - tand(theta3)*outletLength - (1/3)*outletLength*m);
+            d = m/(3*outletLength^2) - 2*c/(3*outletLength);
+            y(i) = a + b*(x(i) - L1) + c*(x(i) - L1)^2 + d*(x(i) - L1)^3;
+        end
+
+    end
+    A = [x', y'];
+    T = array2table(A, "VariableNames",{'Length (m)', 'Distance from Centerline (m)'});
+    thetaStr = string(AngleforLength(j));
+    name = 'ND_InletAngle_' + thetaStr+'.xlsx';
+    writetable(T, name);
 end
+
+
+
